f840d21a1e882f95a1d2385e1b37ad2f0d7905de
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2019-05-31 11:08:14 +0700

    After activity

--- WIP-XI/src/XI/Project/WIPCargo.java
+++ WIP-XI/src/XI/Project/WIPCargo.java
@@ -1,51 +1,53 @@
 /*
  * WIP XI Computer Programing 
  */
-
 package XI.Project;
+
 import java.util.Scanner;
+
 /**
  *
  * @author sittiwatlcp
  */
 public class WIPCargo {
+
     public static int item = 0;
     public static int limit = 10;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("1.Add \n2.Remove \n3.Check");
         System.out.print("เลือกเมนู: ");
         int menu = sc.nextInt();
-        if(menu == 1){
-            if(item < limit){
+        if (menu == 1) {
+            if (item < limit) {
                 System.out.print(" ต้องการเพิ่มกี่ไอเท็ม: ");
                 int amount = sc.nextInt();
-                if(item + amount > limit){
+                if (item + amount > limit) {
                     System.out.println(" ไอเท็มเกินลิมิต");
-                }else{
+                } else {
                     item += amount;
                     System.out.println("ITEM: " + item);
                     System.out.println("LIMIT: " + limit);
                 }
-            }else{
+            } else {
                 System.out.println("ไอเท็มเต็มแล้ว");
             }
-        }
-        else if(menu == 2){
-            if(item != 0){
+        } else if (menu == 2) {
+            if (item != 0) {
                 System.out.print(" ต้องการลบกี่ไอเท็ม: ");
                 int amount = sc.nextInt();
-                if(amount <= item){
+                if (amount <= item) {
                     item -= amount;
                     System.out.println("ITEM: " + item);
                     System.out.println("LIMIT: " + limit);
-                }else{
+                } else {
                     System.out.println("ลบไม่ได้เพราะไอเท็มที่เหลืออยู่ไม่เพียงพอ");
                 }
-            }else{
+            } else {
                 System.out.println("ไอเท็มไม่มีให้ลบแล้ว");
             }
-        }else if(menu == 3){
+        } else if (menu == 3) {
             System.out.println("ไอเท็มในโกดัง: " + item);
         }
         System.out.println("--------------------------------");
---
+++ WIP-XI/src/XI/Project/WIPtestr.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019 WipCamp11.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package XI.Project;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ *
+ * @author WipCamp11
+ */
+public class WIPtestr extends WIPUtil{
+    public static void main(String[] args){
+        print("Hello");
+    }
+}
+
+class WIPUtil{
+    public static void print(Object o){
+        System.out.print(o);
+    }
+    public static void println(Object o){
+        System.out.println(o);
+        int[] x = [1,8,6,4,3,8,2,6,4,1];
+        Arrays.sort(a,new Comparator<int>(
+                
+        ));
+    }
+}
+
+
